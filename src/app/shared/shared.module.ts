c0dc02b5022ca06354eaf5f30828fd794ed5e0f0
NileshJorwar <jorwarnilesh@gmail.com>
2022-06-07 06:38:36 -0500

    separared tabs components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,21 +2,27 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  providers: [ModalService] //array of services
+  // providers: [ModalService] //array of services
 
 })
 export class SharedModule { }
