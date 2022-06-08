2f88b43a8969550ceca83786a3c6ab2e623feb8d
NileshJorwar <jorwarnilesh@gmail.com>
2022-06-08 03:51:33 -0500

    preventing default behavior

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -10,27 +10,27 @@ export class TabsContainerComponent implements AfterContentInit {
 
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
 
-   constructor() { }
+  constructor() { }
 
   ngAfterContentInit(): void {
-   
+
     const activeTabs = this.tabs?.filter(
       tab => tab.active
     );
 
-    if(!activeTabs || activeTabs.length ==0)
-    {
+    if (!activeTabs || activeTabs.length == 0) {
       this.selectTab(this.tabs!.first)
     }
 
   }
 
-  selectTab(tab: TabComponent){
+  selectTab(tab: TabComponent) {
     this.tabs?.forEach
-    (
-      tab=> {tab.active = false} 
-    )
-  
-  tab.active = true;
-}
+      (
+        tab => { tab.active = false }
+      )
+
+    tab.active = true;
+    return false;
+  }
 }
